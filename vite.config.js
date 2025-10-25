e5e52cef569a9f0121fa238200510802d29d7ee7
Sivesh <2300030863@kluniversity.in>
2025-10-25 22:16:52 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   // Use base path for production builds, root for development
-  base: command === 'build' ? '/expense-tracker/' : '/',
+  base: command === 'build' ? './' : '/',
   plugins: [react()],
   server: {
     port: 3000,
